fix normalisation v2

src/electron_defect_interaction/defects/local_R.py
@@ -319,7 +319,7 @@ def compute_ML_R_mpi_shared(uc_wfk_path, sc_wfk_path, sc_p_pot_path, sc_d_pot_pa
     node.Barrier()
     win_u.Sync(); win_v.Sync()
 
-    N_cells = np.prod(Ndiag)
+    N_cells = np.prod(meta["Ndiag"])
     Bk = nb * nk
     dV = Omega_sc / Ntot
     inv_sqrtO = 1.0 / np.sqrt(Omega_sc/N_cells)

src/electron_defect_interaction/wavefunctions/fold_wfk_to_sc.py
@@ -90,7 +90,7 @@ def compute_psi_nk_fold_sc(
         u = np.fft.ifftn(C_grid, axes=(1,2,3)) * N # (nband, Nx, Ny, Nz), N undoes the normzalisation of ifftn
         
         # Compute psi = u * exp(ik.r) / sqrt(Omega)
-        psi[:, ik, ...] = (u * phase_k) / np.sqrt(float(Omega_sc)/N)
+        psi[:, ik, ...] = (u * phase_k) / np.sqrt(float(Omega_sc)/np.prod(Ndiag))
 
     if check_normalize:
         # Sanity check, wavefunctions are nornmalized